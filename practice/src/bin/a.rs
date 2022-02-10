2b64b04a68797e7d0d085929a5e8d804f8eabac2
toming <tomingtoming@gmail.com>
2022-02-10 23:07:12 +0900

    solve practice/a

--- practice/src/bin/a.rs
+++ practice/src/bin/a.rs
@@ -1,3 +1,11 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+        s: String,
+    }
+    println!("{} {}", a + b + c, s);
 }
