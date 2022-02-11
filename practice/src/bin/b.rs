616623a340914033c8cfe80ab0f5b9fa564fd61c
toming <tomingtoming@gmail.com>
2022-02-11 20:21:12 +0900

    solve practice-b

--- practice/src/bin/b.rs
+++ practice/src/bin/b.rs
@@ -1,3 +1,39 @@
+use std::{cmp::Ordering, io::Write};
+
 fn main() {
-    todo!();
+    let mut buf = String::new();
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+
+    buf.clear();
+    stdin.read_line(&mut buf).unwrap();
+    let mut split = buf.trim().split_whitespace();
+    let n: usize = split.next().unwrap().parse().unwrap();
+    let _q: usize = split.next().unwrap().parse().unwrap();
+
+    // prepare character vec
+    let mut chars = vec![
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ];
+    chars.truncate(n);
+
+    chars.sort_unstable_by(|left, right| {
+        println!("? {} {}", &left, &right);
+        stdout.flush().ok().unwrap();
+        buf.clear();
+        stdin.read_line(&mut buf).unwrap();
+        return match buf.trim() {
+            "<" => Ordering::Less,
+            ">" => Ordering::Greater,
+            _ => panic!("unknown response received!"),
+        };
+    });
+
+    print!("! ");
+    for c in chars {
+        print!("{}", c);
+    }
+    print!("\n");
+    stdout.flush().ok().unwrap();
 }
