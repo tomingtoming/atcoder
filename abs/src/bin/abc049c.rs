da2e7ae537dd0408786fe5cc76d600d4fbf1db17
toming <tomingtoming@gmail.com>
2022-02-14 09:04:13 +0900

    solve ABC049C

--- abs/src/bin/abc049c.rs
+++ abs/src/bin/abc049c.rs
@@ -1,3 +1,31 @@
+use proconio::{input, marker::Bytes};
+
 fn main() {
-    todo!();
+    let candidates: Vec<&[u8]> = vec!["dream", "dreamer", "erase", "eraser"]
+        .iter()
+        .map(|s| s.as_bytes())
+        .collect();
+    input! {
+        target: Bytes,
+    }
+    if judge(&target, 0, &candidates) {
+        println!("YES");
+    } else {
+        print!("NO");
+    }
+}
+
+fn judge(target: &Vec<u8>, index: usize, candidates: &Vec<&[u8]>) -> bool {
+    if target.len() == index {
+        return true;
+    }
+    for candidate in candidates {
+        let (_, right) = target.split_at(index);
+        if right.starts_with(candidate) {
+            if judge(target, index + candidate.len(), candidates) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
