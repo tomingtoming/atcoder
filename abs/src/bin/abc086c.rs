0e65754b2f0327df95039cc9c543bf932aae8e71
toming <tomingtoming@gmail.com>
2022-02-15 13:32:28 +0900

    solve: ABC086C - Traveling

--- abs/src/bin/abc086c.rs
+++ abs/src/bin/abc086c.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        targets: [(i32,i32,i32); n],
+    }
+    let mut ti = 0;
+    let mut xi = 0;
+    let mut yi = 0;
+    for (t, x, y) in targets {
+        let dt = t - ti;
+        let dx = x - xi;
+        let dy = y - yi;
+        if (dt + dx + dy) % 2 == 0 && dx.abs() + dy.abs() <= dt {
+            // reachable
+            ti = t;
+            xi = x;
+            yi = y;
+        } else {
+            // unreachable
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
